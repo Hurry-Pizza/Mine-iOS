0fe15cd3aa8ca50d5ee4672bd8f945151c4b2308
고병학 <qkwl4678@naver.com>
2022-08-20 00:40:48 +0900

    [init] 🎉

---
+++ HurryPizza-iOS/HurryPizza_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HurryPizza_iOSApp.swift
+//  HurryPizza-iOS
+//
+//  Created by 고병학 on 2022/08/20.
+//
+
+import SwiftUI
+
+@main
+struct HurryPizza_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
