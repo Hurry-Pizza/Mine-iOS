519ba68f3527e58eaac6544ef0a909a2a287a9b8
bengHak <qkwl4678@naver.com>
2022-08-20 17:43:29 +0900

    [Feat] calculate keyboard height

---
+++ HurryPizza-iOS/Extensions/Publisher+Ext.swift
@@ -0,0 +1,23 @@
+//
+//  Publisher+Ext.swift
+//  HurryPizza-iOS
+//
+//  Created by 고병학 on 2022/08/20.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+extension Publishers {
+    static var keyboardHeight: AnyPublisher<CGFloat, Never> {
+        let willShow = NotificationCenter.default.publisher(for: UIApplication.keyboardWillShowNotification)
+            .map { $0.keyboardHeight }
+        
+        let willHide = NotificationCenter.default.publisher(for: UIApplication.keyboardWillHideNotification)
+            .map { _ in CGFloat(0) }
+        
+        return MergeMany(willShow, willHide)
+            .eraseToAnyPublisher()
+    }
+}
