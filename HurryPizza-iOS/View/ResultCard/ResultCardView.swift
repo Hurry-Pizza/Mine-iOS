82c14d6fe2dd0628a7b5fc49771a462d64ea3b7b
bengHak <qkwl4678@naver.com>
2022-08-20 17:43:46 +0900

    [Feat] Implement signin UI

---
+++ HurryPizza-iOS/View/ResultCard/ResultCardView.swift
@@ -0,0 +1,20 @@
+//
+//  ResultCardView.swift
+//  HurryPizza-iOS
+//
+//  Created by 고병학 on 2022/08/20.
+//
+
+import SwiftUI
+
+struct ResultCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ResultCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultCardView()
+    }
+}
