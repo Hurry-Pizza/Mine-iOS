2b7fb6fb035e60880b0c0bb720591d2666e2eb56
Lawn <user.lawn@gmail.com>
2022-08-20 22:17:05 +0900

    [Feat] Color extension

---
+++ HurryPizza-iOS/Utils/Extentions.swift
@@ -0,0 +1,20 @@
+//
+//  ColorExtensions.swift
+//  HurryPizza-iOS
+//
+//  Created by Jun.Mac on 2022/08/20.
+//
+
+import SwiftUI
+
+extension Color {
+    static let junctionBlack = Color("junctionBlack")
+    static let junctionWhite = Color("junctionWhite")
+    static let junctionGray = Color("junctionGray")
+    static let junctionRed = Color("junctionRed")
+    static let junctionBlue = Color("junctionBlue")
+    static let junctionYellow = Color("junctionYellow")
+    static let junctionGreen = Color("junctionGreen")
+    static let junctionSecondGreen = Color("junctionSecondGreen")
+    static let junctionThirdGreen = Color("junctionThirdGreen")
+    }
